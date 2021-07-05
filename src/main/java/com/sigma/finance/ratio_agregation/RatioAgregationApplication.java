a8b531bafa5848af87ff8dd8dc284c5bc8977a5b
shaobig <54940461+shaobig@users.noreply.github.com>
2021-07-05 14:37:10 +0300

    ra-01: primary preparation

---
+++ src/main/java/com/sigma/finance/ratio_agregation/RatioAgregationApplication.java
@@ -0,0 +1,12 @@
+package com.sigma.finance.ratio_agregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatioAgregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatioAgregationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sigma/finance/ratio_agregation/RatioAgregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigma.finance.ratio_agregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatioAgregationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
